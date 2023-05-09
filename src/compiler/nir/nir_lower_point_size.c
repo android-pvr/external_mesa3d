8561f20e01358b8b6e598d14b06749b4212b16c1
SoroushIMG <soroush.kashani@imgtec.com>
2023-05-09 22:13:49 +0100

    nir: skip non-output derefs
    cast derefs can exist in vulkan and without this check, var can be null.
    
    Signed-off-by: SoroushIMG <soroush.kashani@imgtec.com>

--- src/compiler/nir/nir_lower_point_size.c
+++ src/compiler/nir/nir_lower_point_size.c
@@ -45,6 +45,9 @@ lower_point_size_instr(nir_builder *b, nir_instr *instr, void *data)
       return false;
 
    nir_deref_instr *deref = nir_src_as_deref(intr->src[0]);
+   if (!nir_deref_mode_is(deref, nir_var_shader_out))
+      return false;
+
    nir_variable *var = nir_deref_instr_get_variable(deref);
    if (var->data.location != VARYING_SLOT_PSIZ)
       return false;
