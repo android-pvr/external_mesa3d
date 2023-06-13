fcedac2be837dcef8ecab6601a9341f70f6c1a7b
Simon Perretta <simon.perretta@imgtec.com>
2023-06-13 16:34:23 +0100

    pvr: Amend workgroup size calculation
    Signed-off-by: Simon Perretta <simon.perretta@imgtec.com>

--- src/imagination/vulkan/pvr_cmd_buffer.c
+++ src/imagination/vulkan/pvr_cmd_buffer.c
@@ -4457,7 +4457,8 @@ void pvr_compute_update_kernel_private(
        */
       coeff_regs = dev_runtime_info->cdm_max_local_mem_size_regs;
    } else {
-      coeff_regs = pipeline->coeff_regs_count;
+      coeff_regs =
+         pipeline->coeff_regs_count + pipeline->const_shared_regs_count;
    }
 
    info.usc_common_size =
@@ -4466,8 +4467,6 @@ void pvr_compute_update_kernel_private(
 
    /* Use a whole slot per workgroup. */
    work_size = MAX2(work_size, ROGUE_MAX_INSTANCES_PER_TASK);
-
-   coeff_regs += pipeline->const_shared_regs_count;
 
    if (pipeline->const_shared_regs_count > 0)
       info.sd_type = PVRX(CDMCTRL_SD_TYPE_USC);
@@ -4540,7 +4539,8 @@ static void pvr_compute_update_kernel(
        */
       coeff_regs = dev_runtime_info->cdm_max_local_mem_size_regs;
    } else {
-      coeff_regs = shader_state->coefficient_register_count;
+      coeff_regs = shader_state->coefficient_register_count +
+                   shader_state->const_shared_reg_count;
    }
 
    info.usc_common_size =
@@ -4549,8 +4549,6 @@ static void pvr_compute_update_kernel(
 
    /* Use a whole slot per workgroup. */
    work_size = MAX2(work_size, ROGUE_MAX_INSTANCES_PER_TASK);
-
-   coeff_regs += shader_state->const_shared_reg_count;
 
    if (shader_state->const_shared_reg_count > 0)
       info.sd_type = PVRX(CDMCTRL_SD_TYPE_USC);
